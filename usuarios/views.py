cf5902e2376727d36e40f33c802d54be95ff8287
Julio Benjamim Bernardo <juliobenjamimbernardo36@gmail.com>
2025-03-15 11:09:40 +0200

    cod

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,8 @@ from django.views import View
 from django.http import HttpResponse
 from .forms import UsuarioForm
 import requests
+import xml.etree.ElementTree as ET
+from .models import Usuario
 
 
 class cadastro (View):
@@ -25,9 +27,11 @@ class cadastro (View):
     def _get_codinome(self, grupo):
         if grupo == 'V':
             response = requests.get('https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/vingadores.json')
+            codinomes =[i['codinome']for i in response['vingadores']]
         elif grupo =='LJ':
-            response = request.get('https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml') 
+            response = requests.get('https://raw.githubusercontent.com/uolhost/test-backEnd-Java/master/referencias/liga_da_justica.xml') 
             root = ET.fromstring(response.content)
             codinomes_element = root.find('codinomes')
-            codinomes = [codinomes.text for codinomes in codinomes_element.findall('codinome')]
-
+            codinomes = [codinomes.text for codinome in codinomes_element.findall('codinome')]
+ 
+        codinomes_usados = Usuario.objects.value_list('codinomes', flat=True)
